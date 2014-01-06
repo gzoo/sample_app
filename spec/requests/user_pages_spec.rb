bb71c73562b625cbc09f39614a729e475cf58da4
yuki <gzoo2200@gmail.com>
2014-01-06 13:07:55 +0900

    Progress 10.5 exercise

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,18 +54,36 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
-
-    before { visit user_path(user) }
+    before do
+      31.times { FactoryGirl.create(:micropost, user: user) } 
+      sign_in user
+      visit user_path(user)
+    end
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
 
     describe "microposts" do
-      it { should have_content(m1.content) }
-      it { should have_content(m2.content) }
+      it { should have_selector('div.pagination') }
       it { should have_content(user.microposts.count) }
+
+      it "should have 30 contents at page 1" do
+        count = 0
+        user.microposts.paginate(page: 1).each do |micropost|
+          expect(page).to have_selector('li', text: micropost.content)
+          count += 1
+        end
+        expect(count).equal? 30
+      end
+
+      it "should have 1 content at page 2" do
+        count = 0
+        user.microposts.paginate(page: 2).each do |micropost|
+          expect(page).to have_selector('li', text: micropost.content)
+          count += 1
+        end
+        expect(count).equal? 1
+      end
     end
   end
 
@@ -159,6 +177,20 @@ describe "User pages" do
         patch user_path(user), params
       end
       specify { expect(user.reload).not_to be_admin }
+    end
+  end
+
+  describe "other users pages" do
+    let(:user_me) { FactoryGirl.create(:user) }
+    let(:user_other) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user_other, content: "Foo") }
+    before do
+      sign_in user_me
+      visit user_path(user_other)
+    end
+
+    describe "delete links" do
+      it { should_not have_link('delete') }
     end
   end
 end
